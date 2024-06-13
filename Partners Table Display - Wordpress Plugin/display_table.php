b43ef7d895da9c301f9fd0f49e0291171323749b
Siddhant Sharma <55033732+SiddhantGSharma@users.noreply.github.com>
2024-06-13 11:43:47 -0500

    Add files via upload

---
+++ Partners Table Display - Wordpress Plugin/display_table.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Partners Table Display
+ * Description: A plugin to display partner details from a custom table based on current date.
+ * Version: 1.0
+ * Author: Siddhant Sharma
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Include the shortcodes and other functionalities
+include(plugin_dir_path(__FILE__) . 'includes/shortcodes.php');
